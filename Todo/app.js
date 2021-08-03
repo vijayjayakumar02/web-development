a95a9cc479ed593432311e40f0be2bffdca42cba
vijayjayakumar02 <vijayjayakumar0206@gmail.com>
2021-08-03 17:40:48 +0530

    commits

--- Todo/app.js
+++ Todo/app.js
@@ -1,16 +1,64 @@
-let nodeList = document.getElementsByTagName("li");
-for (var i = 0; i < nodeList.length; i++) {
-  let span = document.createElement("span");
-  let text = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(text);
-  nodeList[i].appendChild(span);
+const inputBox = document.querySelector(".inputField input");
+const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+const deleteBtn = document.querySelector(".footer button");
+
+inputBox.onkeyup = () => {
+  let userValue = inputBox.value;
+  console.log(userValue);
+  if (userValue.trim() != 0) {
+    addBtn.classList.add("active");
+  } else {
+    addBtn.classList.remove("active");
+  }
+};
+showTask();
+addBtn.onclick = () => {
+  let userEnteredValue = inputBox.value;
+  let getLocalStorage = localStorage.getItem("new todo");
+  if (userEnteredValue == 0) {
+    window.alert("Todo list is empty");
+  } else if (getLocalStorage == null) {
+    listArray = [];
+  } else if (userEnteredValue != 0) {
+    listArray.push(userEnteredValue);
+    localStorage.setItem("new todo", JSON.stringify(listArray));
+    showTask();
+    addBtn.classList.remove("active");
+  } else {
+    listArray = JSON.parse(getLocalStorage);
+  }
+};
+function showTask() {
+  let getLocalStorage = localStorage.getItem("new todo");
+  if (getLocalStorage == null) {
+    listArray = [];
+  } else {
+    listArray = JSON.parse(getLocalStorage);
+  }
+  const pendingTasks = document.querySelector(".pendingTasks");
+  pendingTasks.textContent = listArray.length;
+  if (listArray.length < 0) {
+    deleteBtn.classList.add("active");
+  } else {
+    deleteBtn.classList.remove("active");
+  }
+  let newListTag = "";
+  listArray.forEach((element, index) => {
+    newListTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
+  });
+  todoList.innerHTML = newListTag;
+  inputBox.value = "";
 }
-var close = document.getElementsByClassName("close");
-for (var i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    var div = this.parentElement;
-    div.style.display = "none";
-  };
+function deleteTask(index) {
+  let getLocalStorageData = localStorage.getItem("new todo");
+  listArray = JSON.parse(getLocalStorageData);
+  listArray.splice(index, 1);
+  localStorage.setItem("new todo", JSON.stringify(listArray));
+  showTask();
 }
-document.getElementById("count").innerHTML = nodeList.length
+deleteBtn.onclick = () => {
+  listArray = [];
+  localStorage.setItem("new todo", JSON.stringify(listArray));
+  showTask();
+};
