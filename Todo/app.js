49f6ba6ec1f05dfd340ae70cb2077c75e0f94f33
vijayjayakumar02 <vijayjayakumar0206@gmail.com>
2021-08-03 08:18:18 +0530

    commits

---
+++ Todo/app.js
@@ -0,0 +1,15 @@
+let nodeList = document.getElementsByTagName("li");
+for (var i = 0; i < nodeList.length; i++) {
+  let span = document.createElement("span");
+  let text = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(text);
+  nodeList[i].appendChild(span);
+}
+var close = document.getElementsByClassName("close");
+for (var i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
