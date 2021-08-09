5cf71c9c51f9b6bf92b382462f282130db3850bf
vijayjayakumar02 <vijayjayakumar0206@gmail.com>
2021-08-09 09:59:01 +0530

    commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './component/todos/todos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TodosComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/component/todos/todos.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent implements OnInit {
+  todos!: Todo[];
+
+  inputTodo: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'First Todo',
+        completed: false,
+      },
+      {
+        content: 'Second Todo',
+        completed: false,
+      },
+      {
+        content: 'Third Todo',
+        completed: false,
+      },
+    ];
+  }
+  Done(id: number) {
+    this.todos.map((value, index) => {
+      if (index == id) value.completed = !value.completed;
+      return value;
+    });
+  }
+  Delete(id: number) {
+    this.todos = this.todos.filter((value, index) => index !== id);
+  }
+  Add() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false,
+    });
+    this.inputTodo = '';
+  }
+}
